c0aa0392e558798bbc7f35445cc16d393e7102a4
Saddam <eatdatcode@gmail.com>
2018-12-15 12:49:41 +0530

    Add files via upload

---
+++ getDatWord.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3.7
+"""
+file: dict.py
+author: @github.com/eatDatCode
+Takes a single word as command line argument and gives
+Definition with example
+"""
+
+import re
+import sys
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+def getDatWord(word):
+    """This method search for the word in dictionay.com and returns \
+            the definition with a suitable example"""
+    # This string forms the url from which the data will be scrapped
+    url = 'https://www.dictionary.com/browse/'+word+'?s/t'
+
+    # Uses the library urllib and scraps the source code of the url
+    html = urlopen(url)
+
+    # Beautifies the html source code using BeautifulSoup library
+    bs = BeautifulSoup(html.read(),'html.parser')
+
+    # Find all the tags where the definition is in dictionary.com
+    wordLine = bs.findAll('li',{'value':'1'})
+
+    # Seperate the definition and example
+    defRegx = re.compile(r'.+:')
+    exampleRegx = re.compile(r':.+')
+    definitionData = wordLine[0].get_text()
+
+    definition = defRegx.findall(definitionData)
+    example = exampleRegx.findall(definitionData)
+
+    # Check whether there is an example or not
+    if(len(example) == 0):
+        definition = definitionData[:-1]
+        example = "---"
+    else:
+        definition = definition[0][:-1]
+        example = example[0][2:]
+
+    # Sends the dat to printWord methods to be printed
+    printWord(definition,example)
+
+
+def printWord(defn,exmpl):
+    """This method simply prints the word definition and example \
+            by formatting the strings"""
+    print("Definition: %s." % defn.capitalize())
+    print("Example: %s " % exmpl.capitalize())
+
+
+if __name__=='__main__':
+    """ This method initiates the program , taking input from command line\
+            then calling the getDatWord() method to give output"""
+    word = sys.argv[1]
+
+    # Calls the method to find the word definition
+    getDatWord(word)
